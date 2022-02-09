bfa32301d6856ac371c93912697a133ee393ac04
DhruviSutariya36 <98315599+DhruviSutariya36@users.noreply.github.com>
2022-02-09 11:06:53 +0530

    Add files via upload

---
+++ C#/Assignment1/Assignment1/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    class Person
+    {
+        private string _name;
+
+        public string Name
+        {
+            
+            get
+            {
+                return _name;
+            }
+            
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Name cannot be empty");
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        
+        private int _age;
+
+        
+        public int Age
+        {
+            
+            get
+            {
+                return _age;
+            }
+            
+            set
+            {
+                if (value >= 18 && value <= 65)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Age has to be 18 and 65");
+                }
+            }
+        }
+
+    }
+}
---
+++ C#/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Console.Write("Enter your name: ");
+            person.Name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            person.Age = int.Parse( Console.ReadLine() );
+
+            Console.WriteLine();
+            Console.WriteLine("Your name is {0} and your age is {1}", person.Name, person.Age); ;
+        }
+    }
+}
